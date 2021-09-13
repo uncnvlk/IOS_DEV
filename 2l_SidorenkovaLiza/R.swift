44615f2e9e9da8a532def312c4e0d915b323e697
lisa <elizavetasidorenkova@MacBook-Air-Elizaveta.local>
2021-09-13 18:00:10 +0300

    homework-2

---
+++ 2l_SidorenkovaLiza/R.swift
@@ -0,0 +1,14 @@
+//
+//  R.swift
+//  2l_SidorenkovaLiza
+//
+//  Created by Elizaveta Sidorenkova on 13.09.2021.
+//
+
+import UIKit
+
+enum R {
+    enum Storyboard {
+        static let debugMenu: UIStoryboard = .init(name: "DebugMenu", bundle: nil)
+    }
+}
