4f2049377cdf7b2606ba1315e0a7480abd88b2be
lisa <elizavetasidorenkova@MacBook-Air-Elizaveta.local>
2021-09-16 16:43:04 +0300

    homework-3

--- 2l_SidorenkovaLiza/R.swift
+++ 2l_SidorenkovaLiza/R.swift
@@ -11,4 +11,12 @@ enum R {
     enum Storyboard {
         static let debugMenu: UIStoryboard = .init(name: "DebugMenu", bundle: nil)
     }
+    
+    enum Nib {
+        static let basic: UINib = .init(nibName: "BasicTableViewCell", bundle: nil)
+    }
+    
+    enum Cell {
+        static let basic: String = "BasicCell"
+    }
 }
